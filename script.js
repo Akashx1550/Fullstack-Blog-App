087eb48c920e20d672e04c8706801efe8094e413
Akash <101383490+Akashx1550@users.noreply.github.com>
2024-03-19 01:22:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,12 +21,18 @@ const port = process.env.PORT || 3334;
 
 app.use('/', require('./routes/blog'));
 
-mongoose.connect('mongodb://127.0.0.1:27017/G17Session').then(() => {
-    app.listen(port, () => {
-        console.log(`Server started on ${port}`);
-    })
-})
+// mongoose.connect('mongodb://127.0.0.1:27017/G17Session').then(() => {
+//     app.listen(port, () => {
+//         console.log(`Server started on ${port}`);
+//     })
+// })
 
-// app.listen(port, () => {
-//        console.log(`Server started on ${port}`);
-//    })
+mongoose.connect('mongodb+srv://akashx1550:gD6z9GxH9MudGUVe@cluster0.4kkprau.mongodb.net/')
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server started on ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
